2372bb581fb2ac12b1c1cc9ca83e1f75957c2e19
Yaroslav Oliinyk <yaroslav.oliinyk.lviv@gmail.com>
2025-05-26 15:01:27 +0300

    init

---
+++ src/test/java/com/oliinyk/yaroslav/SpringBootWithThymeleafExample/SpringBootWithThymeleafExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oliinyk.yaroslav.SpringBootWithThymeleafExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithThymeleafExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
